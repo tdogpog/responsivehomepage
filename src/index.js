1ebf3d6d9bad6494ddf92a81424a3f46b66d237f
tdog <161662619+tdogpog@users.noreply.github.com>
2024-10-03 19:25:57 -0500

    rough header, html for main body

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles.css";
+
+import imgSrc from "./pexels-abdullah-alallah-314142096-28558771.jpg";
+
+const imgElement = document.querySelector(".imgbox img");
+imgElement.src = imgSrc;
